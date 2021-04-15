d2b0cd8636df92c314dfaf0cb734841289bbac5d
aakash <aakash>
2021-04-15 15:32:08 +0530

    Update

--- pepcode/TSP2/Level1/1.Basics of Programming/Pattern/Pattern11.java
+++ pepcode/TSP2/Level1/1.Basics of Programming/Pattern/Pattern11.java
@@ -1,24 +1,18 @@
 import java.util.Scanner;
 
-public class Pattern3 {
-
+class Pattern11 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int counter = 1;
         int stars = 1;
-        int spaces = n - 1;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < spaces; j++) {
-                // no of spaces
-                System.out.print("\t");
-            }
-            // no of stars
             for (int j = 0; j < stars; j++) {
-                System.out.print("*\t");
+                System.out.print(counter + "\t");
+                counter++;
             }
             stars++;
-            spaces--;
+
             System.out.println();
         }
     }
