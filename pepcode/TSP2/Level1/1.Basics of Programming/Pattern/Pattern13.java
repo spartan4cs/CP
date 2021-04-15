0d8f3bcece726edfab474991fcbb28107fa2ec4e
aakash <aakash>
2021-04-15 16:10:34 +0530

    Update

---
+++ pepcode/TSP2/Level1/1.Basics of Programming/Pattern/Pattern13.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int ans = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ans + "\t");
+                ans = (ans * (i - j)) / (j + 1);
+            }
+            System.out.println();
+        }
+
+    }
+}
