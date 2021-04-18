5106e2176d9cf011df136ffe74da1c6a6bed9688
aakash <aakash>
2021-04-18 14:03:24 +0530

    Update

--- pepcode/TSP2/Level1/2.Recursion And Backtracking/recursion with arraylist/GetStairPaths.java
+++ pepcode/TSP2/Level1/2.Recursion And Backtracking/recursion with arraylist/GetStairPaths.java
@@ -11,19 +11,29 @@ public class GetStairPaths {
         } else if (n < 0) {
             return new ArrayList<>();
         }
-        ArrayList<String> p1 = getStaitPath(n - 1);
-        ArrayList<String> p2 = getStaitPath(n - 2);
-        ArrayList<String> p3 = getStaitPath(n - 3);
+        // ArrayList<String> p1 = getStaitPath(n - 1);
+        // ArrayList<String> p2 = getStaitPath(n - 2);
+        // ArrayList<String> p3 = getStaitPath(n - 3);
+        // ArrayList<String> paths = new ArrayList<>();
+
+        // for (String p : p1) {
+        // paths.add(1 + p);
+        // }
+        // for (String p : p2) {
+        // paths.add(2 + p);
+        // }
+        // for (String p : p3) {
+        // paths.add(3 + p);
+
+        // }
+
         ArrayList<String> paths = new ArrayList<>();
 
-        for (String p : p1) {
-            paths.add(1 + p);
-        }
-        for (String p : p2) {
-            paths.add(2 + p);
-        }
-        for (String p : p3) {
-            paths.add(3 + p);
+        for (int jump = 1; jump <= 3 && n - jump >= 0; jump++) {
+            ArrayList<String> p = getStaitPath(n - jump);
+            for (String pString : p) {
+                paths.add(jump + pString);
+            }
 
         }
         return paths;
