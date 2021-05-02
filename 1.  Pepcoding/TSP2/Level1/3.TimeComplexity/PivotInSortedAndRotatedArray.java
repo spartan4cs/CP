7befb6f1af3a50eccc1808feef3da20e670f8b95
aakash <aakash>
2021-05-02 13:13:34 +0530

    Update

--- 1.  Pepcoding/TSP2/Level1/3.TimeComplexity/PivotInSortedAndRotatedArray.java
+++ 1.  Pepcoding/TSP2/Level1/3.TimeComplexity/PivotInSortedAndRotatedArray.java
@@ -1,3 +1,54 @@
+import java.util.Scanner;
+
 public class PivotInSortedAndRotatedArray {
-    
+
+    // recursive
+    public static int findPivotRec(int[] arr, int lo, int hi) {
+        // write your code here
+
+        if (lo == hi)
+            return arr[lo];
+
+        int mid = lo + (hi - lo) / 2;
+        int res = 0;
+
+        if (arr[mid] < arr[hi]) {
+            res = findPivotRec(arr, lo, mid);
+        } else {
+            res = findPivotRec(arr, mid + 1, hi);
+        }
+
+        return res;
+    }
+
+    // iterative
+    public static int findPivot(int[] arr) {
+        // write your code here
+
+        int lo = 0;
+        int hi = arr.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] < arr[hi]) {
+                // leftside->including mid
+                hi = mid;
+
+            } else {
+                // right side
+                lo = mid + 1;
+            }
+        }
+        return arr[lo];
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int pivot = findPivot(arr);
+        System.out.println(pivot);
+    }
 }
