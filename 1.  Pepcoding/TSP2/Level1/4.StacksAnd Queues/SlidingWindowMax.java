3efbd3fbcf3054225507c8f7fed6241965e55fc9
aakash <aakash>
2021-05-08 22:36:46 +0530

    Updaey

--- 1.  Pepcoding/TSP2/Level1/4.StacksAnd Queues/SlidingWindowMax.java
+++ 1.  Pepcoding/TSP2/Level1/4.StacksAnd Queues/SlidingWindowMax.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Stack;
 
 public class SlidingWindowMax {
     public static void main(String[] args) throws Exception {
@@ -11,6 +12,38 @@ public class SlidingWindowMax {
         }
         int k = Integer.parseInt(br.readLine());
 
+        // nge to right
+
+        int nge[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        nge[n - 1] = n;
+        st.push(0);
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            while (st.size() > 0 && a[i] >= a[st.peek()]) {
+                st.pop();
+            }
+            if (st.empty()) {
+                nge[i] = n;
+
+            } else {
+                nge[i] = st.peek();
+            }
+            st.push(i);
+        }
+
         // code
+
+        for (int i = 0; i < a.length - k; i++) {
+
+            int j = i;
+
+            while (j < i + k) {
+                j = nge[j];
+            }
+            System.out.println(a[j]);
+        }
     }
 }
