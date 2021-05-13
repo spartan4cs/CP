0ee4bd5d06fb7c2a6666db2008e1d76ee8ea61c4
aakash <aakash>
2021-05-13 22:01:51 +0530

    Update

---
+++ 1.  Pepcoding/TSP2/Level1/4.StacksAnd Queues/GFG_GenerateBinaryNumber.java
@@ -0,0 +1,3 @@
+public class GFG_GenerateBinaryNumber {
+    
+}
--- 1.  Pepcoding/TSP2/Level1/4.StacksAnd Queues/MinimumStack.java
+++ 1.  Pepcoding/TSP2/Level1/4.StacksAnd Queues/MinimumStack.java
@@ -3,6 +3,52 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class MinimumStack {
+
+    // using single stack
+    class MinStack2 {
+
+        /** initialize your data structure here. */
+        private Stack<int[]> st;
+
+        public MinStack2() {
+            st = new Stack<>();
+        }
+
+        public void push(int val) {
+            // if the stack is empty, the min value should be the val itself
+            if (st.isEmpty()) {
+                int[] arr = new int[2];
+                arr[0] = val;
+                arr[1] = val;
+                st.push(arr);
+            }
+            // now, compare the existed min val to this new added val
+            else {
+                int[] arr = new int[2];
+                arr[0] = val;
+                arr[1] = Math.min(getMin(), val);
+                st.push(arr);
+            }
+
+        }
+
+        public void pop() {
+            st.pop();
+        }
+
+        public int top() {
+            int[] arr = st.peek();
+            return arr[0];
+        }
+
+        public int getMin() {
+            // the current min value for the elements in the stacks currently
+            // always store in the top element
+            return st.peek()[1];
+        }
+    }
+
+    // using 2 stack
     public static class MinStack {
         Stack<Integer> allData;
         Stack<Integer> minData;
