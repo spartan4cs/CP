b148b762fa322fd3b7720147ed63ca95b7b697f7
aakash <aakash>
2021-05-16 02:51:20 +0530

    update

--- 2.Pepcoding/TSP2/Level1/2.Recursion And Backtracking/recursion with backtracking/PinBall.java
+++ 2.Pepcoding/TSP2/Level1/2.Recursion And Backtracking/recursion with backtracking/PinBall.java
@@ -1,5 +1,66 @@
 public class PinBall {
 
+    // normal direction
+    public static int[] rdir = { -1, 0, 1, 0 };
+    public static int[] cdir = { 0, -1, 0, 1 };
+    public static char[] charr = { 't', 'l', 'd', 'r' };
+
+    // backward obstacle direction (l->t ,t->l, r->d, d->r) \
+
+    public static int[] rdirb = { 0, -1, 0, 1 };
+    public static int[] cdirb = { -1, 0, 1, 0 };
+    public static char[] charrb = { 'l', 't', 'r', 'd' };
+
+    // forward obstacle dirction (l->d, d->l,r->t,t->r) /
+    public static int[] rdirf = { 0, 1, 0, -1 };
+    public static int[] cdirf = { 1, 0, -1, 0 };
+    public static char[] charrf = { 'r', 'd', 'l', 't' };
+
+    // direction approach
+    public static void traverse_dir(char[][] board, int sr, int sc, int dr, int dc, char currDir, String asf,
+            String coordinates) {
+
+        if (sr == -1 || sc == -1 || sr >= board.length || sc >= board[0].length) {
+
+            System.out.println(asf);
+            System.out.println(coordinates);
+
+            return;
+        }
+
+        if (sr >= 0 && sc >= 0 && sr <= dr && sc <= dc) {
+
+            int nr = 0;
+            int nc = 0;
+            char ndir = 'l';
+            // t or l or d or right jau shakto
+            // jar i is your current direction - la jaar ahe
+            for (int i = 0; i < charr.length; i++) {
+
+                if (currDir == charr[i]) {
+                    if (board[sr][sc] == '\\') {// obstacle 1 ala tar
+                        nr = sr + rdirb[i]; // backward obstacle direction
+                        nc = sc + cdirb[i];
+                        ndir = charrb[i];
+
+                    } else if (board[sr][sc] == '/') { // obstacle 2 tar
+                        nr = sr + rdirf[i];// forward obstacle direction
+                        nc = sc + cdirf[i];
+                        ndir = charrf[i];
+                    } else {
+                        nr = sr + rdir[i]; // normal direction
+                        nc = sc + cdir[i];
+                        ndir = charr[i];
+                    }
+                }
+
+            }
+            traverse_dir(board, nr, nc, dr, dc, ndir, asf + ndir, coordinates + "(" + sr + "," + sc + ")");
+
+        }
+    }
+
+    // allcals checking
     public static void traverse(char[][] board, int sr, int sc, int dr, int dc, char currDir, String asf,
             String coordinates) {
 
@@ -13,7 +74,7 @@ public class PinBall {
 
         if (sr >= 0 && sc >= 0 && sr <= dr && sc <= dc) {
 
-            if (board[sr][sc] == '\\') {// obstacle 1
+            if (board[sr][sc] == '\\') {// obstacle 1 (l->t ,t->l, r->d, d->r)
                 if (currDir == 'l') {// based on current direction i am changing the direction
                     traverse(board, sr - 1, sc, dr, dc, 't', asf + "t", coordinates + "(" + sr + "," + sc + ")");
                 } else if (currDir == 't') {
@@ -23,7 +84,7 @@ public class PinBall {
                 } else if (currDir == 'd') {
                     traverse(board, sr, sc + 1, dr, dc, 'r', asf + "r", coordinates + "(" + sr + "," + sc + ")");
                 }
-            } else if (board[sr][sc] == '/') {// obstacle 2
+            } else if (board[sr][sc] == '/') {// obstacle 2(l->d, d->l,r->t,t->r)
                 if (currDir == 'l') {// based on current direction i am changing the direction
                     traverse(board, sr + 1, sc, dr, dc, 'd', asf + "d", coordinates + "(" + sr + "," + sc + ")");
                 } else if (currDir == 't') {
@@ -87,6 +148,8 @@ public class PinBall {
 
         };
         traverse(board, 2, 3, 2, 3, 'l', "", "");
+        traverse_dir(board, 2, 3, 2, 3, 'l', "", "");
+
         // for (int i = 0; i < board.length; i++) {
         // for (int j = 0; j < board[0].length; j++) {
         // System.out.println(board[i][j] + " ");
