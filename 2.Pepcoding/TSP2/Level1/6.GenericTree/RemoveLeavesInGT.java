f616fb61a858f50b6dd228c20e9ca73560f57a6a
aakash <aakashkumbhare@gmail.com>
2021-05-28 01:23:37 +0530

    Updated code on timestamp:   28-05-2021 -  1:23:36.93

--- 2.Pepcoding/TSP2/Level1/6.GenericTree/RemoveLeavesInGT.java
+++ 2.Pepcoding/TSP2/Level1/6.GenericTree/RemoveLeavesInGT.java
@@ -133,7 +133,17 @@ public class RemoveLeavesInGT {
 
     public static void removeLeaves(Node node) {
         // write your code here
-        
+        for (int i = node.children.size() - 1; i >= 0; i--) {
+            Node child = node.children.get(i);
+            if (child.children.size() == 0) {
+                node.children.remove(i);
+            }
+        }
+
+        for (Node child : node.children) {
+            removeLeaves(child);
+        }
+
     }
 
     public static void main(String[] args) throws Exception {
