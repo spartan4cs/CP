5264737d5b9dbd68cc390ba2f7f6b34f6c5558fd
aakash <aakashkumbhare@gmail.com>
2021-06-13 01:20:50 +0530

    Updated code on timestamp:   13-06-2021 -  1:20:49.65

---
+++ 2.Pepcoding/TSP2/Level1/8.BST/TargetSumPair.java
@@ -0,0 +1,139 @@
+
+import java.io.*;
+import java.util.*;
+
+public class TargetSumPair {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static Node construct(Integer[] arr) {
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+
+        int idx = 0;
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+
+                top.state++;
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+
+        return root;
+    }
+
+    public static void display(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        String str = "";
+        str += node.left == null ? "." : node.left.data + "";
+        str += " <- " + node.data + " -> ";
+        str += node.right == null ? "." : node.right.data + "";
+        System.out.println(str);
+
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            if (values[i].equals("n") == false) {
+                arr[i] = Integer.parseInt(values[i]);
+            } else {
+                arr[i] = null;
+            }
+        }
+
+        int data = Integer.parseInt(br.readLine());
+
+        Node root = construct(arr);
+        // write your code here
+
+            targetSum1(root, root, data);
+        // targetSum2();
+        // targetSum3();
+    }
+
+    // Appraoch 1
+    // Time nh Space h
+    public static void targetSum1(Node node, Node root, int target) {
+
+        if (node == null) {
+            return;
+        }
+        int n1 = node.data;
+        int n2 = target - n1;
+
+        targetSum1(node.left, root, target);
+
+        // inorder
+        if (n2 > n1 && find(root, n2) == true) {
+            System.out.println(n1 + " " + n2);
+        }
+        targetSum1(node.right, root, target);
+    }
+
+    public static boolean find(Node node, int data) {
+
+        if (node == null)
+            return false;
+
+        if (data > node.data) {
+            return find(node.right, data);
+        } else if (data < node.data) {
+            return find(node.left, data);
+        } else {
+            return true;
+        }
+    }
+
+}
