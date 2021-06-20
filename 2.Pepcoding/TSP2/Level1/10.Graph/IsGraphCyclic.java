e43e8b65ded4afaa6f8e5393ed55de293d4ecd6e
aakash <aakashkumbhare@gmail.com>
2021-06-21 00:24:20 +0530

    Updated code on timestamp:   21-06-2021 -  0:24:19.60

---
+++ 2.Pepcoding/TSP2/Level1/10.Graph/IsGraphCyclic.java
@@ -0,0 +1,85 @@
+
+import java.io.*;
+import java.util.*;
+
+public class IsGraphCyclic {
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+            String[] parts = br.readLine().split(" ");
+            int v1 = Integer.parseInt(parts[0]);
+            int v2 = Integer.parseInt(parts[1]);
+            int wt = Integer.parseInt(parts[2]);
+            graph[v1].add(new Edge(v1, v2, wt));
+            graph[v2].add(new Edge(v2, v1, wt));
+        }
+
+        // write your code here
+        System.out.println(isCyclic(graph));
+
+    }
+
+    public static boolean isCyclic(ArrayList<Edge>[] graph) {
+
+        int n = graph.length;
+        boolean vis[] = new boolean[n];
+
+        for (int v = 0; v < n; v++) {
+            if (vis[v] == false) {
+                boolean res = bfs(graph, v, vis);
+                if (res) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean bfs(ArrayList<Edge>[] graph, int src, boolean vis[]) {
+
+        Queue<Integer> qu = new LinkedList<>();
+        qu.add(src);
+        while (qu.size() > 0) {
+
+            // rmwa
+
+            Integer rem = qu.remove();
+
+            // mark
+
+            if (vis[rem] == true) {
+                // cycle dected
+                return true;
+            }
+            vis[rem] = true;
+
+            // add
+            for (Edge e : graph[rem]) {
+                if (vis[e.nbr] == false) {
+                    qu.add(e.nbr);
+                }
+            }
+        }
+        return false;
+    }
+}
