86442b312e2800c9d8159378161cc5fcb4447f5c
aakash <aakashkumbhare@gmail.com>
2021-06-21 00:58:27 +0530

    Updated code on timestamp:   21-06-2021 -  0:58:26.59

---
+++ 2.Pepcoding/TSP2/Level1/10.Graph/SpreadOfInfection.java
@@ -0,0 +1,40 @@
+
+import java.io.*;
+import java.util.*;
+
+public class SpreadOfInfection {
+    static class Edge {
+        int src;
+        int nbr;
+
+        Edge(int src, int nbr) {
+            this.src = src;
+            this.nbr = nbr;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+            String[] parts = br.readLine().split(" ");
+            int v1 = Integer.parseInt(parts[0]);
+            int v2 = Integer.parseInt(parts[1]);
+            graph[v1].add(new Edge(v1, v2));
+            graph[v2].add(new Edge(v2, v1));
+        }
+
+        int src = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
+
+        // write your code here
+    }
+
+}
