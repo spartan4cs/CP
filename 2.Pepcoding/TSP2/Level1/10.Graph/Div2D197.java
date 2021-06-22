5a3d26c8b51df56fb1dbcab436514646c07b2c52
aakash <aakashkumbhare@gmail.com>
2021-06-23 03:11:32 +0530

    Updated code on timestamp:   23-06-2021 -  3:11:32.06

---
+++ 2.Pepcoding/TSP2/Level1/10.Graph/Div2D197.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Div2D197 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        char[][] arr = new char[n][m];
+        int si = 0;
+        int sj = 0;
+        for (int i = 0; i < n; i++) {
+
+            String s = sc.next();
+            for (int j = 0; j < s.length(); j++) {
+                arr[i][j] = s.charAt(j);
+                if (arr[i][j] == 'S') {
+                    si = i;
+                    sj = j;
+                }
+            }
+
+        }
+
+        boolean ans = getAns(arr, si, sj);
+        // for (int i = 0; i < n; i++) {
+        // for (int j = 0; j < m; j++) {
+        // System.out.println(arr[i][j]);
+        // }
+        // }
+
+    }
+
+    static int xdir[] = { -1, 0, 1, 0 };
+    static int ydir[] = { 0, -1, 0, 1 };
+
+    public static boolean getAns(char[][] arr, int si, int sj) {
+
+        for (int d = 0; d < 4; d++) {
+            int ni = si + xdir[d];
+            int nj = sj + ydir[d];
+            if (ni == -1) {
+                ni = arr.length - 1;
+            }
+            if (nj == -1) {
+                nj = arr[0].length - 1;
+            }
+            if (ni == arr.length) {
+                ni = 0;
+            }
+            if (nj == arr[0].length) {
+                nj = 0;
+            }
+            if (arr[ni][nj] == '.' || arr[i][j] == 'S') {
+                getAns(arr, ni, nj);
+            }
+        }
+    }
+}
