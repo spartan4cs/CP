46b281b48e8241f2b682aa36f7f0e42ebaa9ccce
aakash <aakashkumbhare@gmail.com>
2021-06-26 14:31:33 +0530

    Updated code on timestamp:   26-06-2021 - 14:31:33.19

--- 2.Pepcoding/TSP2/Level1/11.DP/GoldMine.java
+++ 2.Pepcoding/TSP2/Level1/11.DP/GoldMine.java
@@ -1,3 +1,58 @@
+import java.util.*;
+import java.io.*;
+
 public class GoldMine {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        // code
+
+        int res = -(int) 1e9;
+        for (int i = 0; i < n; i++) {
+
+            int ans = recursion(arr, i, 0, n, m);
+            res = Math.max(ans, res);
+
+        }
+        System.out.println(res);
+    }
+
+    public static int recursion(int[][] arr, int x, int y, int n, int m) {
+
+        if (y == m - 1) {
+            return arr[x][y];
+        }
+
+        int maxGold = -(int) 1e9;
+        if (x == 0) {
+
+            int a = recursion(arr, x, y + 1, n, m);
+            int b = recursion(arr, x + 1, y + 1, n, m);
+            maxGold = Math.max(maxGold, Math.max(a, b));
+        } else if (x == n - 1) {
+
+            int a = recursion(arr, x - 1, y + 1, n, m);
+            int b = recursion(arr, x, y + 1, n, m);
+            maxGold = Math.max(maxGold, Math.max(a, b));
+
+        } else {
+
+            int a = recursion(arr, x - 1, y + 1, n, m);
+            int b = recursion(arr, x, y + 1, n, m);
+            int c = recursion(arr, x + 1, y + 1, n, m);
+            maxGold = Math.max(maxGold, Math.max(a, Math.max(b, c)));
+        }
+
+        return maxGold + arr[x][y];
+    }
 }
