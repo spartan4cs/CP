1dd9336ceae893051ecbf56f7b756f94dd6a0e2f
aakash <aakashkumbhare@gmail.com>
2021-06-26 20:22:41 +0530

    Updated code on timestamp:   26-06-2021 - 20:22:40.75

--- 2.Pepcoding/TSP2/Level1/11.DP/GoldMine.java
+++ 2.Pepcoding/TSP2/Level1/11.DP/GoldMine.java
@@ -207,6 +207,7 @@ public class GoldMine {
             for (int x = n - 1; x >= 0; x--) {
                 if (y == m - 1) {
                     qb[x][y] = arr[x][y];
+                    res = Math.max(res, qb[x][y]);
                     continue;
                 }
 
