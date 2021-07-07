366380db0c3395120a1e27e335b86478a2687088
aakash <aakashkumbhare@gmail.com>
2021-07-08 01:38:48 +0530

    Updated code on timestamp:   08-07-2021 -  1:38:47.54

--- 2.Pepcoding/TSP2/Level1/11.DP/PaintFence.java
+++ 2.Pepcoding/TSP2/Level1/11.DP/PaintFence.java
@@ -2,5 +2,22 @@ import java.util.*;
 import java.io.*;
 
 class PaintFence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int same = 0;
+        int distinct = k;
+        for (int i = 2; i <= n; i++) {
+            int n_same = distinct;
+            int n_distinct = (same + distinct) * (k - 1);
+            // update
+            same = n_same;
+            distinct = n_distinct;
+        }
+
+        System.out.println(same + distinct);
+    }
 
 }
