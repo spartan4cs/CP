aefa3abb39befa70d60f32924c876454fd0a562a
aakash <aakashkumbhare@gmail.com>
2021-07-08 02:01:21 +0530

    Updated code on timestamp:   08-07-2021 -  2:01:20.82

---
+++ 2.Pepcoding/TSP2/Level1/11.DP/ArrangeBuilding.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ArrangeBuilding {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int oz = 1;
+        int o1 = 1;
+        for (int i = 2; i <= n; i++) {
+            int nz = o1;
+            int n1 = oz + o1;
+
+            oz = nz;
+            o1 = n1;
+        }
+
+        int ans = oz + o1;
+        System.out.println(ans * ans);
+
+    }
+
+}
