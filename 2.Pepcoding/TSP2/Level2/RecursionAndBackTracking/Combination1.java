d83837ba761ffd416547b0733e18f133aaf1de32
aakash <aakashkumbhare@gmail.com>
2021-07-17 12:19:06 +0530

    Updated code on timestamp:   17-07-2021 - 12:19:06.54

--- 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Combination1.java
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Combination1.java
@@ -28,7 +28,7 @@ public class Combination1 {
 
     // cb-current box, tb-total box, isf - items so far, ti-total items,asf=
     // answersofar
-    public static void combination_sirapproach(int cb, int tb, int isf, int ti, String asf) {
+    public static void combination1(int cb, int tb, int isf, int ti, String asf) {
         // write your code here
 
         if (cb > tb) {
@@ -41,11 +41,36 @@ public class Combination1 {
 
         // yes call
         if (isf + 1 <= ti) {
-            combination_sirapproach(cb + 1, tb, isf + 1, ti, asf + "i");
+            combination1(cb + 1, tb, isf + 1, ti, asf + "i");
         }
 
         // no calls
-        combination_sirapproach(cb + 1, tb, isf, ti, asf + "-");
+        combination1(cb + 1, tb, isf, ti, asf + "-");
+    }
+
+    // ci-> current item, ti-> total item, lb-> last box used
+    public static void combinations2(int[] boxes, int ci, int ti, int lb) {
+        if (ci > ti) {
+            for (int i = 0; i < boxes.length; i++) {
+                if (boxes[i] == 0) {
+                    System.out.print("-");
+                } else {
+                    System.out.print("i");
+                }
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int b = lb; b < boxes.length; b++) {
+            if (boxes[b] == 0) {
+                // place ci item on bth box
+                boxes[b] = ci;
+                combinations2(boxes, ci + 1, ti, b);
+                // unplace
+                boxes[b] = 0;
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -53,7 +78,8 @@ public class Combination1 {
         int nboxes = Integer.parseInt(br.readLine());
         int ritems = Integer.parseInt(br.readLine());
         // combination_myapproach(new int[nboxes], 1, ritems, 0);
-        combination_sirapproach(1, nboxes, 0, ritems, "");
+        // combination1(1, nboxes, 0, ritems, "");
+        combinations2(new int[nboxes], 1, ritems, 0);
     }
 
 }
