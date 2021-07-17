dbcdd4b7a7de557aa0da1700e1b8be74a41b7012
aakash <aakashkumbhare@gmail.com>
2021-07-17 11:54:19 +0530

    Updated code on timestamp:   17-07-2021 - 11:54:18.53

---
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/QueenPermutation.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class QueenPermutation {
+    public static void queensPermutations(int qpsf, int tq, int[][] chess) {
+        // write your code here
+
+        if (qpsf == tq) {
+            for (int i = 0; i < chess.length; i++) {
+                for (int j = 0; j < chess[0].length; j++) {
+                    if (chess[i][j] != 0) {
+                        System.out.print("q" + chess[i][j] + "\t");
+                    } else {
+                        System.out.print("-\t");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < chess.length; i++) {
+            for (int j = 0; j < chess[0].length; j++) {
+                if (chess[i][j] == 0) {
+                    // place queen
+                    chess[i][j] = qpsf + 1;
+                    queensPermutations(qpsf + 1, tq, chess);
+                    chess[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] chess = new int[n][n];
+
+        queensPermutations(0, n, chess);
+    }
+}
