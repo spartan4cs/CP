2f4da6b6e42cae6f57fe65af2458b0c1f3a79760
aakash <aakashkumbhare@gmail.com>
2021-08-05 23:53:38 +0530

    Updated code on timestamp:   05-08-2021 - 23:53:37.38

--- 2.Pepcoding/TSP2/Level2/Linkedlist/Leet328.java
+++ 2.Pepcoding/TSP2/Level2/Linkedlist/Leet328.java
@@ -1,54 +1,60 @@
 import java.util.Scanner;
 
-public class Odd_Even_Linked_List_Leet328 {
+public class Leet328 {
+
     public static class ListNode {
         int val = 0;
         ListNode next = null;
 
-        ListNode(int val) {
+        public ListNode(int val) {
             this.val = val;
         }
     }
 
-    public static ListNode segregateEvenOdd(ListNode head) {
+    public static ListNode oddEvenList(ListNode head) {
+
         ListNode itr = head;
         ListNode t1 = new ListNode(-1);// even
-        ListNode evenhead = t1;
+        ListNode even = t1;
         ListNode t2 = new ListNode(-1);// odd
-        ListNode oddhead = t2;
+        ListNode odd = t2;
 
+        int i = 0;
         while (itr != null) {
-            if (itr.val % 2 == 0) {
+
+            if (i % 2 == 0) {
                 t1.next = itr;
                 t1 = itr;
-
             } else {
                 t2.next = itr;
                 t2 = itr;
-
             }
+
+            i++;
             itr = itr.next;
         }
         t1.next = null;
         t2.next = null;
 
-        t1.next = oddhead.next;
+        // even followed by odd
+        t1.next = odd.next;
 
-        return evenhead.next;
+        return even.next;
 
     }
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
+
+        Scanner sc = new Scanner(System.in);
         ListNode dummy = new ListNode(-1);
         ListNode prev = dummy;
+        int n = sc.nextInt();
         while (n-- > 0) {
-            prev.next = new ListNode(scn.nextInt());
+            prev.next = new ListNode(sc.nextInt());
             prev = prev.next;
         }
 
-        ListNode head = segregateEvenOdd(dummy.next);
+        ListNode head = oddEvenList(dummy.next);
         while (head != null) {
             System.out.print(head.val + " ");
             head = head.next;
