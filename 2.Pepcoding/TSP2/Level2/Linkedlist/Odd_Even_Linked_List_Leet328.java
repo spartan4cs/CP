58eccb7a2f427f5d8251c0110bf491588d8863ef
aakash <aakashkumbhare@gmail.com>
2021-08-05 23:27:49 +0530

    Updated code on timestamp:   05-08-2021 - 23:27:48.68

---
+++ 2.Pepcoding/TSP2/Level2/Linkedlist/Odd_Even_Linked_List_Leet328.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Odd_Even_Linked_List_Leet328 {
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode segregateEvenOdd(ListNode head) {
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        ListNode head = segregateEvenOdd(dummy.next);
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+}
