b73ea927e63bdf7c09e84ee6a8d1a2ebb1a6b48c
Akash Kumbhare <aakashkumbhare@gmail.com>
2021-09-23 23:03:42 +0530

    Update KnightTraversal.java

--- 2.Pepcoding/TSP2/Level1/2.Recursion And Backtracking/recursion with backtracking/KnightTraversal.java
+++ 2.Pepcoding/TSP2/Level1/2.Recursion And Backtracking/recursion with backtracking/KnightTraversal.java
@@ -1,33 +1,76 @@
+import java.io.*;
+import java.util.*;
+
 public class KnightTraversal {
 
-    // Note there will be no radius
-
-    public static void demo() {
-        // matrix 5x4;
-        int n = 8;
-        int m = 8;
-
-        int r = 4; // current r
-        int c = 4; // currnt c
-
-        int[][] dir = {
-
-                { -2, 1 }, { -1, 2 }, { 1, 2 }, { 2, 1 }, { 2, -1 }, { 1, -2 }, { -1, -2 }, { -2, -1 }
-
-        };
-
-        for (int d = 0; d < dir.length; d++) {
-
-            int nr = r + dir[d][0];
-            int nc = c + dir[d][1];
-
-            if (nr >= 0 && nr <= n - 1 && nc >= 0 && nc <= m - 1) {
-                System.out.println("Direction : " + d + " -> " + " ( " + nr + " , " + nc + " )");
+ public static void display(int[][] board) {
+        // System.out.println("------------------start------------");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
             }
+            System.out.println();
         }
+
+        System.out.println();
+        // System.out.println("-----------------end---------------");
     }
 
     public static void main(String[] args) {
-        demo();
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row = sc.nextInt();// start row
+        int col = sc.nextInt();// start col
+        int[][] board = new int[n][n];
+        printKnightsTour(board, row, col, 1);
+    }
+
+    public static int[][] dir = {
+
+             { -2, 1 }, // upper
+
+            { -1, 2 }, { 1, 2 }, // left
+
+          { 2, 1 },   { 2, -1 }, // down
+
+           { 1, -2 } , { -1, -2 }, { -2, -1 }// right
+
+    };
+
+ //public static int[] xdir = {-2, -1, 1, 2, 2, 1, -1, -2};
+  //  public static int[] ydir = {1, 2, 2, 1, -1, -2, -2, -1};
+
+    public static void printKnightsTour(int[][] board, int sr, int sc, int count) {
+
+        if (count == board.length * board.length) {
+
+            board[sr][sc] = count; // for last box
+            display(board);
+            board[sr][sc] = 0;
+            return;
+        }
+
+        // mark
+        board[sr][sc] = count;
+
+        // knight traversal
+        for (int d = 0; d < dir.length; d++) {
+
+            int nr = sr + dir[d][0];
+            int nc = sc + dir[d][1];
+
+            if (nr >= 0 && nr < board.length && nc >= 0 && nc < board.length) {// within board
+
+                if (board[nr][nc] == 0) {// box empty
+
+                    printKnightsTour(board, nr, nc, count + 1);
+                }
+            }
+        }
+
+        // unmark
+        board[sr][sc] = 0;
+
     }
 }
