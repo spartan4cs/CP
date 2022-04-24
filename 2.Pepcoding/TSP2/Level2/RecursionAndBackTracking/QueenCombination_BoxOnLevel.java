447489574c04e60b717d9b202a190fa8acbc6a94
Akash Kumbhare <aakashkumbhare@gmail.com>
2022-04-24 17:06:39 +0530

    Update QueenCombination_BoxOnLevel.java

--- 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/QueenCombination_BoxOnLevel.java
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/QueenCombination_BoxOnLevel.java
@@ -31,6 +31,47 @@ public class QueenCombination_BoxOnLevel {
         }
     }
 
+    
+    //Other way
+    
+       public static void queensCombinations(int qpsf, int tq, int row, int col, String asf){
+        // write your code here
+        
+        //base case
+        if(row==tq){
+            if(qpsf==tq)
+                System.out.println(asf);
+
+            return;
+        }
+        
+        
+        
+        //box on level
+        //option are either yes or no
+        
+        String sep="";
+        int nr=row;
+        int nc=col;
+        if(col<tq-1){
+            nr=row;
+            nc=col+1;
+            sep="";
+        }else{
+            nr=row+1;
+            nc=0;
+            sep="\n";
+        }
+        
+        
+        
+        //yes call
+        queensCombinations(qpsf+1,tq,nr,nc,asf+"q"+sep);
+        
+        //no call
+        queensCombinations(qpsf,tq,nr,nc,asf+"-"+sep);
+
+    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
