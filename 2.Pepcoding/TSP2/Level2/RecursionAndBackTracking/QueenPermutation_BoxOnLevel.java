93a73e101fa021633aefd83f24d673e21e96f550
Akash Kumbhare <aakashkumbhare@gmail.com>
2022-04-24 17:58:31 +0530

    Update QueenPermutation_BoxOnLevel.java

--- 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/QueenPermutation_BoxOnLevel.java
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/QueenPermutation_BoxOnLevel.java
@@ -1,1 +1,56 @@
+import java.io.*;
+import java.util.*;
 
+public class QueenPermutation_BoxOnLevel {
+
+  public static void queensPermutations(int qpsf, int tq, int row, int col, String asf, boolean[] queens) {
+    // write your code here
+
+    //base
+    if (row == tq) {
+      if (qpsf == tq) {
+        System.out.println(asf);
+        System.out.println();
+      }
+      return;
+    }
+
+
+    //box onlevel
+    //yes no call
+    //perm
+
+    int nr = row;
+    int nc = col;
+    String sep = "";
+    if (col == tq - 1) {
+      nr = row + 1;
+      nc = 0;
+      sep = "\n";
+    } else {
+      nr = row;
+      nc = col + 1;
+      sep = "\t";
+    }
+
+    for (int i = 0; i < queens.length; i++) {
+      if (queens[i] == false) {
+        queens[i] = true;
+        queensPermutations(qpsf + 1, tq, nr, nc, asf + "q" + (i + 1) + sep, queens);
+        queens[i] = false;
+      }
+    }
+
+    queensPermutations(qpsf + 0, tq, nr, nc, asf + "-" + sep, queens);
+
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    boolean[] queens = new boolean[n];
+
+    queensPermutations(0, n, 0, 0, "", queens);
+  }
+}
