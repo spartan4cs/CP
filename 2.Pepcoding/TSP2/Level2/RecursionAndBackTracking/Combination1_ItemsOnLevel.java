77bf7daf47307dbb21540967972d1bdb941f3bd6
Akash Kumbhare <aakashkumbhare@gmail.com>
2022-04-24 15:30:49 +0530

    Create Combination1_ItemsOnLevel.java

---
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Combination1_ItemsOnLevel.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void combinations(int[] boxes, int ci, int ti, int lb) {
+    // write your code here
+
+    if (ci > ti) {
+      for (int b = 0; b < boxes.length; b++) {
+        if (boxes[b] == 0) {
+          System.out.print("-");
+
+        } else {
+          System.out.print("i");
+
+        }
+
+      }
+      System.out.println();
+      return;
+    }
+
+    //items on level
+    //there are identical items
+    // i .e on next level for iteration1,you should start from next box
+    for (int b = lb + 1; b < boxes.length; b++) {
+      if (boxes[b] == 0) {
+        boxes[b] = ci;
+        combinations(boxes, ci + 1, ti, b);
+        boxes[b] = 0;
+
+      }
+    }
+
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int nboxes = Integer.parseInt(br.readLine());
+    int ritems = Integer.parseInt(br.readLine());
+    combinations(new int[nboxes], 1, ritems, -1);
+  }
+
+}
