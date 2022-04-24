5ea9ba1936085eccb7b2e63becde90ad4610b74c
Akash Kumbhare <aakashkumbhare@gmail.com>
2022-04-24 16:03:03 +0530

    Update Combination2_BoxOnLevel.java

--- 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Combination2_BoxOnLevel.java
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Combination2_BoxOnLevel.java
@@ -3,13 +3,13 @@ import java.util.*;
 
 public class Main {
 
-  public static void permutations(int cb, int tb, int[] items, int ssf, int ts, String asf) {
+  public static void combinations(int cb, int tb, int ssf, int ts, String asf) {
     // write your code here
 
     if (cb > tb) {
-      //if (ssf == ts) {
-      System.out.println(asf);
-      // }
+      if (ssf == ts) {
+        System.out.println(asf);
+      }
       return;
     }
 
@@ -18,24 +18,17 @@ public class Main {
     //yes and no call
 
     //yes call
-    for (int i = 0; i < items.length; i++) {
-      if (items[i] == 0) {
+    combinations(cb + 1, tb, ssf + 1, ts, asf + "i");
 
-        items[i] = 1;
-        permutations(cb + 1, tb, items, ssf + 1, ts, asf + (i + 1) + "");
-        items[i] = 0;
-      }
-    }
-
-    //nocall
-    permutations(cb + 1, tb, items, ssf, ts, asf + 0 + "");
+    //no call
+    combinations(cb + 1, tb, ssf, ts, asf + "-");
   }
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int nboxes = Integer.parseInt(br.readLine());
     int ritems = Integer.parseInt(br.readLine());
-    permutations(1, nboxes, new int[ritems], 0, ritems, "");
+    combinations(1, nboxes, 0, ritems, "");
   }
 
 }
