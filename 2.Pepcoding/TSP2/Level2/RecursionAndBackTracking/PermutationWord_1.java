19294ca05ee8f141e3ae528f791b1576b4d62f09
Akash <aakashkumbhare@gmail.com>
2022-04-27 15:13:57 +0530

    Updated code on timestamp:   27-04-2022 - 15:13:56.94

---
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/PermutationWord_1.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class PermutationWord_1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        // freq map
+
+        HashMap<Character, Integer> fmap = new HashMap<>();
+        for (Character str : s.toCharArray()) {
+            if (fmap.containsKey(str)) {
+                fmap.put(str, fmap.get(str) + 1);
+            } else {
+                fmap.put(str, 1);
+            }
+        }
+        // for (Character c : fmap.keySet()) {
+        // System.out.println(c + " " + fmap.get(c));
+        // }
+
+        // for (Map.Entry<Character, Integer> entry : fmap.entrySet()) {
+        // System.out.println(entry.getKey() + " " + entry.getValue());
+        // }
+
+        char[] asf = new char[s.length()];
+        permutationWord(fmap, asf, 0, s.length());
+    }
+
+    // cb -current box
+    // tb=total box
+    private static void permutationWord(HashMap<Character, Integer> fmap, char[] asf, int cb, int tb) {
+
+        // base
+        if (cb == tb) {
+            System.out.println(asf);
+        }
+
+        for (Character c : fmap.keySet()) {
+
+            if (fmap.get(c) > 0) {
+
+                asf[cb] = c;
+                fmap.put(c, fmap.get(c) - 1);
+                permutationWord(fmap, asf, cb + 1, tb);
+                asf[cb] = '\0';
+                fmap.put(c, fmap.get(c) + 1);
+            }
+        }
+    }
+}
