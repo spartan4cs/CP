e7c3560a9491f0e4faac6e0e3595e8acc1645d06
Akash <aakashkumbhare@gmail.com>
2022-04-27 19:29:05 +0530

    Updated code on timestamp:   27-04-2022 - 19:29:05.29

--- 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Word_K_Selection_2_LessOnLevel.java
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Word_K_Selection_2_LessOnLevel.java
@@ -9,6 +9,32 @@ public class Word_K_Selection_2_LessOnLevel {
 
         HashMap<Character, Integer> fmap = new HashMap();
 
-        
+        String ustr = "";
+        for (char c : s.toCharArray()) {
+            if (fmap.containsKey(c)) {
+                fmap.put(c, fmap.get(c) + 1);
+            } else {
+                fmap.put(c, 1);
+                ustr += c;
+            }
+        }
+
+        // System.out.println(ustr);
+
+        selection(k, ustr, "", -1);
     }
+
+    private static void selection(int k, String ustr, String asf, int lo) {
+
+        if (k == 0) {
+            System.out.println(asf);
+            return;
+        }
+
+        for (int i = lo + 1; i < ustr.length(); i++) {
+
+            selection(k - 1, ustr, asf + ustr.charAt(i), i);
+        }
+    }
+
 }
