06bc7be31768985c80e64efcec2aefd239dafc1a
Akash <aakashkumbhare@gmail.com>
2022-04-27 19:07:34 +0530

    Updated code on timestamp:   27-04-2022 - 19:07:34.04

--- 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Word_K_Selection_1.java
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Word_K_Selection_1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Word_K_Selection_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next;
+        String s = sc.next();
         int k = sc.nextInt();
 
         // need to select k distinct chars
@@ -21,6 +21,30 @@ public class Word_K_Selection_1 {
                 ustr += c;
             }
         }
-        System.out.println(ustr);
+        // System.out.println(ustr);
+
+        selection(ustr, 0, "", k);
+
+    }
+
+    // cl -current level
+    private static void selection(String ustr, int cl, String asf, int k) {
+        // base
+
+        if (cl == ustr.length()) {
+
+            if (asf.length() == k) {
+                System.out.println(asf);
+            }
+            return;
+        }
+
+        // yes call
+
+        char ch = ustr.charAt(cl);// current character
+        selection(ustr, cl + 1, asf + ch, k);
+
+        // no call
+        selection(ustr, cl + 1, asf, k);
     }
 }
