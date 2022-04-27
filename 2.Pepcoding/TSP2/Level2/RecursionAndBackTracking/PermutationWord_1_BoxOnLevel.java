9c6d4e52d603b23af372becba4fb82a994e7cac8
Akash <aakashkumbhare@gmail.com>
2022-04-27 15:14:58 +0530

    Updated code on timestamp:   27-04-2022 - 15:14:58.30

--- 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/PermutationWord_1_BoxOnLevel.java
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/PermutationWord_1_BoxOnLevel.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class PermutationWord_1 {
+public class PermutationWord_1_BoxOnLevel {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
