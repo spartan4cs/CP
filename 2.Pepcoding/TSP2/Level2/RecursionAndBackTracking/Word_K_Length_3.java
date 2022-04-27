2eb75fbb21140a47f1332c474eb27e33b73b61a8
Akash <aakashkumbhare@gmail.com>
2022-04-28 00:51:36 +0530

    Updated code on timestamp:   28-04-2022 -  0:51:36.19

--- 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Word_K_Length_3.java
+++ 2.Pepcoding/TSP2/Level2/RecursionAndBackTracking/Word_K_Length_3.java
@@ -7,7 +7,41 @@ public class Word_K_Length_3 {
         String str = br.readLine();
         int k = Integer.parseInt(br.readLine());
 
-        
+        HashMap<Character, Integer> loc = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            loc.put(c, -1);
+        }
 
+        perm(str, 0, k, loc, new char[k], 0, k);
+
+    }
+
+    private static void perm(String str, int cl, int k, HashMap<Character, Integer> loc, char[] asf, int ssf, int ts) {
+
+        if (cl == str.length()) {
+            if (ssf == ts) {
+                System.out.println(asf);
+            }
+
+            return;
+        }
+
+        // yes call
+        char ch = str.charAt(cl);
+        int lo = loc.get(ch);
+        for (int i = lo + 1; i < asf.length; i++) {
+            if (asf[i] == '\0') {
+                asf[i] = ch;
+                loc.put(ch, i);
+                perm(str, cl + 1, k, loc, asf, ssf + 1, ts);
+                loc.put(ch, lo);
+                asf[i] = '\0';
+            }
+        }
+        // no call only when there is no occurence
+
+        if (lo == -1) {
+            perm(str, cl + 1, k, loc, asf, ssf, ts);
+        }
     }
 }
