98a8e933dae429c8db591129bd1bef9a639fa26c
Akash <aakashkumbhare@gmail.com>
2022-04-30 21:30:25 +0530

    Updated code on timestamp:   30-04-2022 - 21:30:25.02

--- leetcode/698.partition-to-k-equal-sum-subsets.java
+++ leetcode/698.partition-to-k-equal-sum-subsets.java
@@ -8,27 +8,48 @@ import java.util.Arrays;
 
 // @lc code=start
 class Solution {
-    static boolean flag = false;
+    boolean flag = false;
 
     public boolean canPartitionKSubsets(int[] nums, int k) {
-        solution(nums, 0, k, "", 0, new int[k]);
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        sum = sum / k;
+        solution(nums, n - 1, k, sum, 0, new int[k]);
         return flag;
 
     }
 
     // nos- no of set filled
-    private static void solution(int[] nums, int i, int k, String asf, int nos, int[] ans) {
+    private void solution(int[] nums, int i, int k, int sum, int nos, int[] ans) {
 
-        if (i == nums.length) {
+        if (i == -1) {
             if (nos == k) {
                 // for (int j : ans) {
                 // System.out.print(j + " ");
                 // }
                 // System.out.println();
-                boolean allMatch = Arrays.stream(ans).allMatch(t -> t == ans[0]);
-                if (allMatch) {
-                    flag = true;
+                // boolean allMatch = Arrays.stream(ans).allMatch(t -> t == sum);
+                // if (allMatch) {
+                // flag = true;
 
+                // return;
+                // }
+
+                boolean lflag = true;
+                for (int j = 0; j < ans.length; j++) {
+                    if (sum != ans[j]) {
+                        lflag = false;
+                        break;
+                    }
+
+                }
+                if (lflag) {
+                    flag = true;
                     return;
                 }
             }
@@ -37,18 +58,21 @@ class Solution {
 
         for (int j = 0; j < k; j++) {
 
-            if (ans[j] != 0) {
-                ans[j] += nums[i];
-                solution(nums, i + 1, k, asf, nos, ans);
-                ans[j] -= nums[i];
-            }
-            // empy set
-            if (ans[j] == 0) {
+            if (ans[j] + nums[i] <= sum) {
 
-                ans[j] += nums[i];
-                solution(nums, i + 1, k, asf, nos + 1, ans);
-                ans[j] -= nums[i];
-                break;// to avoid multiple times adding to empty set
+                if (ans[j] != 0) {
+                    ans[j] += nums[i];
+                    solution(nums, i - 1, k, sum, nos, ans);
+                    ans[j] -= nums[i];
+                } else
+                // empy set
+                if (ans[j] == 0) {
+
+                    ans[j] += nums[i];
+                    solution(nums, i - 1, k, sum, nos + 1, ans);
+                    ans[j] -= nums[i];
+                    break;// to avoid multiple times adding to empty set
+                }
             }
         }
     }
