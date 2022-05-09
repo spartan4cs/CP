183e99a2e7bd3a69cb747d31d63f23c6904ded3e
Akash <aakashkumbhare@gmail.com>
2022-05-09 17:47:00 +0530

    Updated code on timestamp:   09-05-2022 - 17:46:59.78

---
+++ leetcode/563.binary-tree-tilt.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=563 lang=java
+ *
+ * [563] Binary Tree Tilt
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findTilt(TreeNode root) {
+        
+    }
+}
+// @lc code=end
+
