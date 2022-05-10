0f1a08c56204d077d108ed7e4dc3487df84946dd
Akash <aakashkumbhare@gmail.com>
2022-05-11 02:46:13 +0530

    Updated code on timestamp:   11-05-2022 -  2:46:13.34

--- leetcode/337.house-robber-iii.java
+++ leetcode/337.house-robber-iii.java
@@ -8,22 +8,48 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
+
+    class pair {
+        int with_rob;
+        int without_rob;
+
+        public pair() {
+            with_rob = 0;
+            without_rob = 0;
+        }
+    }
+
     public int rob(TreeNode root) {
-        
+        pair p = helper(root);
+        return Math.max(p.with_rob, p.without_rob);
+    }
+
+    public pair helper(TreeNode root) {
+
+        if (root == null) {
+            return new pair();
+        }
+        pair l = helper(root.left);
+
+        pair r = helper(root.right);
+
+        pair p = new pair();
+        p.with_rob = root.val + l.without_rob + r.without_rob;
+        p.without_rob = Math.max(l.with_rob, l.without_rob) + Math.max(r.with_rob, r.without_rob);
+        return p;
     }
 }
 // @lc code=end
-
--- leetcode/968.binary-tree-cameras.java
+++ leetcode/968.binary-tree-cameras.java
@@ -8,22 +8,48 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
+    int count = 0;
+
     public int minCameraCover(TreeNode root) {
-        
+        if (helper(root) == -1)
+            count++;
+        return count;
+    }
+
+    // 3 state
+    // -1 = need camera,
+    // 0 ,placed camera,
+    // 1 - iam cover by child
+
+    public int helper(TreeNode root) {
+        if (root == null) {
+            return 1;
+
+        }
+        int l = helper(root.left);
+        int r = helper(root.right);
+
+        if (l == -1 || r == -1) {
+            count++; // increase camera
+            return 0;
+        }
+        if (l == 0 || r == 0) {
+            return 1;
+        }
+        return -1;
     }
 }
 // @lc code=end
-
