b2d450ae240ca3690b8942bc3c30e7b189875893
Akash <aakashkumbhare@gmail.com>
2022-06-16 21:15:55 +0530

    Updated code on timestamp:   16-06-2022 - 21:15:55.41

--- leetcode/297.serialize-and-deserialize-binary-tree.java
+++ leetcode/297.serialize-and-deserialize-binary-tree.java
@@ -8,22 +8,72 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        
+
+        // use stack to iterate bt preorder
+
+        if (root == null) {
+            return null;
+        }
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        List<String> al = new ArrayList<>();
+        while (!st.isEmpty()) {
+
+            // rma
+            // remove
+            TreeNode c = st.pop();
+            if (c == null) {
+                al.add("null");
+            } else {
+
+                // print
+                al.add("" + c.val);
+                // add child(1st right then left bec for preorder traversal, left to be poped
+                // first)
+                st.push(c.right);
+                st.push(c.left);
+            }
+
+        }
+
+        return String.join(",", al);
+
     }
+
+    static int t;
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        
+        if (data == null)
+            return null;
+        t = 0;
+
+        String arr[] = data.split(",");
+
+        return helper(arr);
+    }
+
+    public TreeNode helper(String[] arr) {
+
+        if (arr[t].equals("null")) {
+            t++;
+            return null;
+        }
+
+        TreeNode tn = new TreeNode(Integer.parseInt(arr[t++]));
+        tn.left = helper(arr);
+        tn.right = helper(arr);
+        return tn;
     }
 }
 
@@ -32,4 +82,3 @@ public class Codec {
 // Codec deser = new Codec();
 // TreeNode ans = deser.deserialize(ser.serialize(root));
 // @lc code=end
-
--- leetcode/449.serialize-and-deserialize-bst.java
+++ leetcode/449.serialize-and-deserialize-bst.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /*
  * @lc app=leetcode id=449 lang=java
  *
@@ -5,26 +9,85 @@
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
+// Definition for a binary tree node.
+// public class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+
+//     TreeNode(int x) {
+//         val = x;
+//     }
+// }
+
 public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        
+
+        // use stack to iterate bt preorder
+
+        if (root == null) {
+            return null;
+        }
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        List<String> al = new ArrayList<>();
+        while (!st.isEmpty()) {
+
+            // rma
+            // remove
+            TreeNode c = st.pop();
+
+            if (c != null) {
+
+                // print
+                al.add("" + c.val);
+                // add child(1st right then left bec for preorder traversal, left to be poped
+                // first)
+                st.push(c.right);
+                st.push(c.left);
+            }
+
+        }
+        String ans = String.join(",", al);
+        System.out.println(ans);
+        return ans;
+
     }
+
+    int t;
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        
+
+        if (data == null)
+            return null;
+        t = 0;
+
+        String arr[] = data.split(",");
+
+        return helper(arr, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
+
+    public TreeNode helper(String[] arr, int min, int max) {
+
+        if (t >= arr.length) {
+            return null;
+        }
+        int temp = Integer.parseInt(arr[t]);
+        if ( temp < min || temp > max) {
+
+            return null;
+        }
+
+        TreeNode tn = new TreeNode(Integer.parseInt(arr[t++]));
+        tn.left = helper(arr, min, tn.val);
+        tn.right = helper(arr, tn.val, max);
+        return tn;
+    }
+
 }
 
 // Your Codec object will be instantiated and called as such:
@@ -34,4 +97,3 @@ public class Codec {
 // TreeNode ans = deser.deserialize(tree);
 // return ans;
 // @lc code=end
-
