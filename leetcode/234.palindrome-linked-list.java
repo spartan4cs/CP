663be62d40da21312eddc1112695c7e14a8ca95c
Akash <aakashkumbhare@gmail.com>
2022-06-17 02:04:23 +0530

    Updated code on timestamp:   17-06-2022 -  2:04:23.22

--- leetcode/234.palindrome-linked-list.java
+++ leetcode/234.palindrome-linked-list.java
@@ -8,17 +8,56 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
+    public ListNode getMid(ListNode head) {
+
+        ListNode f=head;
+        ListNode s=head;
+
+        while (f.next!=null && f.next.next!=null) {
+            s=s.next;
+            f=f.next.next;
+        }
+        return s;
+    }
+
+    public ListNode reverse(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
     public boolean isPalindrome(ListNode head) {
-        
+
+        ListNode l1 = head;
+        ListNode mid = getMid(head);
+        ListNode l2 = mid.next;
+        mid.next = null;
+        l2 = reverse(l2);
+
+        while (l2 != null) {
+            if (l1.val != l2.val) {
+                return false;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        return true;
+
     }
 }
 // @lc code=end
-
--- leetcode/98.validate-binary-search-tree.java
+++ leetcode/98.validate-binary-search-tree.java
@@ -8,22 +8,167 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
+
+    // --------approach1 - same logic that you would add for normal sorted arr to
+    // check if
+    // ti is sorted
+
+    // TreeNode prev = null;
+
+    // public boolean isValidBST(TreeNode root) {
+    // // inorder traversal will give sorted list
+    // if (root == null)
+    // return true;
+
+    // boolean leftcall = isValidBST(root.left);
+    // if (!leftcall)
+    // return false;
+
+    // // inorder work
+    // if (prev != null && prev.val >= root.val)
+    // return false;
+    // prev = root;
+
+    // boolean rightcall = isValidBST(root.right);
+    // if (!rightcall)
+    // return false;
+
+    // return true;
+    // }
+
+    // ---------------------approach2 pair
+
+    // class BstPair {
+    // long min;
+    // long max;
+    // boolean isbst;
+
+    // BstPair() {
+    // min = Long.MAX_VALUE;
+    // max = Long.MIN_VALUE;
+    // isbst = true;
+    // }
+    // }
+
+    // public BstPair isBST(TreeNode node) {
+    // if (node == null)
+    // return new BstPair();
+    // BstPair l = isBST(node.left);
+    // BstPair r = isBST(node.right);
+
+    // BstPair mres = new BstPair();
+    // mres.min = Math.min(node.val, Math.min(l.min, r.min));
+    // mres.max = Math.max(node.val, Math.max(l.max, r.max));
+    // mres.isbst = l.isbst && r.isbst && l.max < node.val && r.min > node.val;
+    // return mres;
+
+    // }
+
+    // public boolean isValidBST(TreeNode root) {
+    // // if(root.left==null && root.right==null) return true;
+    // return isBST(root).isbst;
+
+    // }
+
+    // --------------approache 3 using stack iterative BT inorder
+    // public boolean isValidBST(TreeNode root) {
+    // if (root == null)
+    // return true;
+    // Stack<TreeNode> stack = new Stack<>();
+    // TreeNode pre = null;
+    // while (root != null || !stack.isEmpty()) {
+    // //add all left
+    // while (root != null) {
+    // stack.push(root);
+    // root = root.left;
+    // }
+    // //inorder work
+    // root = stack.pop();
+    // if (pre != null && root.val <= pre.val)
+    // return false;
+    // pre = root;
+    // //go to right
+    // root = root.right;
+    // }
+    // return true;
+    // }
+
+    // ------------approach4 Morris traversal
     public boolean isValidBST(TreeNode root) {
-        
+
+        TreeNode prev = null;
+
+        while (root != null) {
+
+            TreeNode left = root.left;
+
+            if (left == null) {
+
+                // at this point(both inorder OR preoredr)
+                if (prev != null && prev.val >= root.val) {
+                    return false;
+                }
+                prev = root;
+                // print
+                System.out.println(root.val);
+
+                // goto right
+                root = root.right;
+            } else {
+
+                // get left ka extremen right
+                TreeNode extremeRight = getExtremeRight(left, root);
+
+                if (extremeRight.right == null) {
+                    // create thread;
+                    extremeRight.right = root;
+
+                    // at this point (preorder)
+                    // go to left
+                    root = root.left;
+                } else {
+
+                    // destroy thread
+                    extremeRight.right = null;
+
+                    if (prev != null && prev.val >= root.val) {
+                        return false;
+                    }
+                    prev = root;
+                    // at this point (inorder)
+                    // print
+                    System.out.println(root.val);
+                    // got to right
+                    root = root.right;
+
+                }
+
+            }
+        }
+        return true;
+
     }
+
+    public TreeNode getExtremeRight(TreeNode head, TreeNode curr) {
+
+        while (head.right != null && head.right != curr) {
+            head = head.right;
+        }
+        return head;
+    }
+
 }
 // @lc code=end
-
