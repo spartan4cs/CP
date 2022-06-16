6a58da6b0005c0484ba1535367fda58b10a0d74f
Akash <aakashkumbhare@gmail.com>
2022-06-16 17:27:06 +0530

    Updated code on timestamp:   16-06-2022 - 17:27:06.09

--- leetcode/235.lowest-common-ancestor-of-a-binary-search-tree.java
+++ leetcode/235.lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * @lc app=leetcode id=235 lang=java
  *
@@ -5,20 +8,58 @@
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
+//  Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+
+        ArrayList<TreeNode> pData = ntr(root, p);
+        ArrayList<TreeNode> qData = ntr(root, q);
+
+        // System.out.println(pData);
+        // System.out.println(qData);
+
+        int i = pData.size() - 1;
+        while (pData.get(i).val == qData.get(i).val) {
+
+            i--;
+        }
+        i++;
+        return pData.get(i);
+    }
+
+    public List<TreeNode> ntr(TreeNode root, TreeNode p) {
+
+        if (root.val == p.val) {
+            List<TreeNode> b_ans = new ArrayList<>();
+            b_ans.add(p.val);
+            return b_ans;
+        }
+        if (root.left != null) {
+            List<TreeNode> l = ntr(root.left, p);
+            if (l.size() > 0) {
+                l.add(root);
+                return l;
+            }
+        }
+        if (root.right != null) {
+            List<TreeNode> r = ntr(root.right, p);
+            if (r.size() > 0) {
+                r.add(root);
+                return r;
+            }
+        }
+        return new ArrayList<>();
     }
 }
 // @lc code=end
-
