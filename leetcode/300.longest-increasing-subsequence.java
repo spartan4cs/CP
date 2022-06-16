f2fae561c0db2355de10f6b325427459b69c7972
Akash <aakashkumbhare@gmail.com>
2022-06-16 12:50:14 +0530

    Updated code on timestamp:   16-06-2022 - 12:50:14.60

---
+++ leetcode/300.longest-increasing-subsequence.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=300 lang=java
+ *
+ * [300] Longest Increasing Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
---
+++ leetcode/312.burst-balloons.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=312 lang=java
+ *
+ * [312] Burst Balloons
+ */
+
+// @lc code=start
+class Solution {
+    public int maxCoins(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
