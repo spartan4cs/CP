35ced05f4c91f465909183f9ef170de2e6b921f2
Akash <aakashkumbhare@gmail.com>
2022-06-18 01:24:03 +0530

    Updated code on timestamp:   18-06-2022 -  1:24:03.72

---
+++ leetcode/1388.pizza-with-3-n-slices.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1388 lang=java
+ *
+ * [1388] Pizza With 3n Slices
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSizeSlices(int[] slices) {
+        
+    }
+}
+// @lc code=end
+
--- leetcode/438.find-all-anagrams-in-a-string.java
+++ leetcode/438.find-all-anagrams-in-a-string.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /*
  * @lc app=leetcode id=438 lang=java
  *
@@ -6,9 +11,56 @@
 
 // @lc code=start
 class Solution {
+    public boolean isEqualArr(int[] a, int[] b) {
+        for (int i = 0; i < b.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public List<Integer> findAnagrams(String s, String p) {
-        
+
+        int[] sHash = new int[26];
+        int[] pHash = new int[26];
+
+        int window = p.length();
+        List<Integer> al = new ArrayList<>();
+        if (s.length() < p.length()) {
+            return al;
+        }
+
+        // sliding window of size p and compare shash with phash
+
+        // create initial window
+
+        int l = 0;
+        int r = 0;
+        while (r < window) {
+            sHash[s.charAt(r) - 'a'] += 1;
+            pHash[p.charAt(r) - 'a'] += 1;
+            r++;
+        }
+
+        r--;
+
+        while (r < s.length()) {
+
+            if (isEqualArr(sHash, pHash)) {
+                al.add(l);
+            }
+
+            r++;// add from end
+            if (r != s.length())
+                sHash[s.charAt(r) - 'a'] += 1;
+
+            sHash[s.charAt(l) - 'a'] -= 1;
+            l++;// remove from start
+
+        }
+
+        return al;
     }
 }
 // @lc code=end
-
