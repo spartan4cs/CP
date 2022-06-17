44d1a7d62e91e661b24a75d420c482a9aca49915
Akash <aakashkumbhare@gmail.com>
2022-06-18 02:11:40 +0530

    Updated code on timestamp:   18-06-2022 -  2:11:40.02

---
+++ leetcode/141.linked-list-cycle.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=141 lang=java
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null) {
+            return false;
+        }
+        // slow fast approach
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+// @lc code=end
---
+++ leetcode/160.intersection-of-two-linked-lists.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=160 lang=java
+ *
+ * [160] Intersection of Two Linked Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+    }
+}
+// @lc code=end
+
