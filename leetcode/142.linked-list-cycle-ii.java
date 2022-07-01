3efa8d27fe0dc8ffdfdc7d738336a6a45c39929c
spartan4cs <aakashkumbhare@gmail.com>
2022-07-01 14:48:57 +0530

    Updated code on timestamp:   01-07-2022 - 14:48:57.35

--- leetcode/142.linked-list-cycle-ii.java
+++ leetcode/142.linked-list-cycle-ii.java
@@ -19,6 +19,27 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         
+
+            
+//   https://www.youtube.com/watch?v=dpqpgnTHiLs]
+    //slow will not complete cycle and fast will complete cycle and catch slow at the metting point
+    //this is true for case where ratiio of fast and slow =2 and for the remaining it may happen that slow completes the cycle
+        
+        ListNode slow= head;
+        ListNode fast= head;
+        
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                while(head!=slow){
+                    head=head.next;
+                    slow=slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
     }
 }
 // @lc code=end
---
+++ leetcode/907.sum-of-subarray-minimums.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=907 lang=java
+ *
+ * [907] Sum of Subarray Minimums
+ */
+
+// @lc code=start
+class Solution {
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+        
+    }
+}
+// @lc code=end
+
