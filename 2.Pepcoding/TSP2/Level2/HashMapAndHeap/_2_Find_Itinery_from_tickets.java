95cd8182664185a6ce78a651a8b919b59e145ef3
spartan4cs <aakashkumbhare@gmail.com>
2022-07-17 00:34:52 +0530

    Updated code on timestamp:   17-07-2022 -  0:34:52.64

---
+++ 2.Pepcoding/TSP2/Level2/HashMapAndHeap/_2_Find_Itinery_from_tickets.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class _2_Find_Itinery_from_tickets {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int noofpairs_src_des = scn.nextInt();
+        HashMap<String, String> map = new HashMap<>();
+        for (int i = 0; i < noofpairs_src_des; i++) {
+            String s1 = scn.next();
+            String s2 = scn.next();
+            map.put(s1, s2);
+        }
+
+        // write your code here
+
+    }
+}
