cbe17b936dbc0088a1a1b350e270e5cb846bb414
Akash <aakashkumbhare@gmail.com>
2024-09-11 23:28:56 +0530

    Updated code on timestamp:   Wed 09/11/2024 - 23:28:56.32

---
+++ leetcode/28.find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=28 lang=java
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+// @lc code=start
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+
+    }
+}
+// @lc code=end
+
