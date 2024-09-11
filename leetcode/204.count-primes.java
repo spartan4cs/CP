b33e2f704f437df6135c49815d0b9b5447f453f5
Akash <aakashkumbhare@gmail.com>
2024-09-11 21:46:39 +0530

    Updated code on timestamp:   Wed 09/11/2024 - 21:46:39.63

---
+++ leetcode/204.count-primes.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=204 lang=java
+ *
+ * [204] Count Primes
+ */
+
+// @lc code=start
+class Solution {
+    public int countPrimes(int n) {
+        
+    }
+}
+// @lc code=end
+
