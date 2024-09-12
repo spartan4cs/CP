df1736ccc0d724df5b362015d91785f708cb8b01
Akash <aakashkumbhare@gmail.com>
2024-09-12 23:44:11 +0530

    Updated code on timestamp:   Thu 09/12/2024 - 23:44:10.74

---
+++ leetcode/992.subarrays-with-k-different-integers.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=992 lang=java
+ *
+ * [992] Subarrays with K Different Integers
+ */
+
+// @lc code=start
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        
+    }
+}
+// @lc code=end
+
