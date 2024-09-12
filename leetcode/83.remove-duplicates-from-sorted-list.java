09dd8ffe6f73438de3b13a1da0253a225cbb9d32
Akash <aakashkumbhare@gmail.com>
2024-09-12 23:16:38 +0530

    Updated code on timestamp:   Thu 09/12/2024 - 23:16:37.64

--- leetcode/83.remove-duplicates-from-sorted-list.java
+++ leetcode/83.remove-duplicates-from-sorted-list.java
@@ -8,17 +8,44 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        
+        if (head != null && head.next != null) {
+
+            ListNode prev = head;
+            ListNode curr = head.next;
+
+            while (curr != null) {
+                if (prev.val == curr.val) {
+                    curr = curr.next;
+                } else {
+                    prev.next = curr;
+                    prev = prev.next;
+                    curr = curr.next;
+                }
+            }
+            prev.next = curr;
+        }
+        return head;
     }
+
+    // recursive
+/*
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+*/
+
+
 }
 // @lc code=end
-
