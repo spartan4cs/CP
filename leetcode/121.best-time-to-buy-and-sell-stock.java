1f699aa25596bf1a17fc7c3c868c0ad299450897
Akash <aakashkumbhare@gmail.com>
2024-09-13 00:54:06 +0530

    Updated code on timestamp:   Fri 09/13/2024 -  0:54:05.98

---
+++ leetcode/121.best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=121 lang=java
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+
+        int minPrice = prices[0]; 
+        int maxProfit = 0; 
+
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit; // Return the maximum profit.
+
+    }
+}
+// @lc code=end
