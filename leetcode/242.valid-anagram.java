adf9f1a82966f1fa79eb69d86ea7824110936351
Akash <aakashkumbhare@gmail.com>
2024-09-13 12:51:02 +0530

    Updated code on timestamp:   Fri 09/13/2024 - 12:51:02.26

---
+++ leetcode/242.valid-anagram.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=242 lang=java
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+    }
+}
+// @lc code=end
+
