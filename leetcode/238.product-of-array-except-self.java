50f89c6771aa5b60bf64df89258b5957bf97e1e9
Akash <aakashkumbhare@gmail.com>
2024-09-13 12:38:54 +0530

    Updated code on timestamp:   Fri 09/13/2024 - 12:38:54.07

--- leetcode/238.product-of-array-except-self.java
+++ leetcode/238.product-of-array-except-self.java
@@ -7,8 +7,25 @@
 // @lc code=start
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
+        // jatana 1st loop madhe
+        // result madhe left product save kar
+
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        for (int i = 1; i < result.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        // yetana 2nd loop
+        // result la right product sobat multiply kar
+        // right product update kar
+
+        int rp = 1;
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = result[i] * rp;
+            rp = rp * nums[i];
+        }
+        return result;
     }
 }
 // @lc code=end
-
