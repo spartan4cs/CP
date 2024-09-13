09f5adfbe89084384cda4ef1728b76ed36eb6d20
Akash <aakashkumbhare@gmail.com>
2024-09-13 16:53:09 +0530

    Updated code on timestamp:   Fri 09/13/2024 - 16:53:09.69

--- leetcode/242.valid-anagram.java
+++ leetcode/242.valid-anagram.java
@@ -7,8 +7,38 @@
 // @lc code=start
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] a = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            int in_s = s.charAt(i) - 'a';
+            int in_t = t.charAt(i) - 'a';
+            a[in_s] += 1;
+            a[in_t] -= 1;
+
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 0) {
+                return false;
+            }
+        }
+        return true;
     }
+
+    //other approach
+    // class Solution {
+    //     public boolean isAnagram(String s, String t) {
+    //         char[] sChars = s.toCharArray();
+    //         char[] tChars = t.toCharArray();
+            
+    //         Arrays.sort(sChars);
+    //         Arrays.sort(tChars);
+            
+    //         return Arrays.equals(sChars, tChars);
+    //     }
+    // }
 }
 // @lc code=end
-
